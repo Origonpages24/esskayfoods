6823a99ab6418468439c3aa7c988649364df485a
Origonpages24 <web.origon@gmail.com>
2024-05-21 18:02:15 +0530

    initial commit

---
+++ navbar.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg">
+  <div class="container-lg">
+    <a class="navbar-brand me-auto" href="index.php"><img src="Assets\logo.png" alt="logo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src="Assets\logo.png" alt="logo"></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-left flex-grow-1 pe-3" style="padding-left: 20px">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Shop
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="shop.php">Coffee</a></li>
+              <li><a class="dropdown-item" href="shop.php">Pappadam</a></li>
+              <li><a class="dropdown-item" href="shop.php">Vadam</a></li>
+              <li><a class="dropdown-item" href="shop.php">Karuvadam</a></li>
+              <li><a class="dropdown-item" href="shop.php">Kondattam</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blogs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+    <div class="cart-menu">
+            <a href="cart.php"><i class="bi bi-bag-heart"></i>Cart </a>
+            <div class="cart-count">0</div>
+        </div>
+  </div>
+</nav>
---
+++ shop.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="bootstrap-5.3.3-dist\css\bootstrap.css">
+  <link rel="stylesheet" href="bootstrap-5.3.3-dist\js\bootstrap.js">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="shortcut icon" href="assets\logo.png" type="image/x-icon">
+  <title>ESSKAY FOODS | Quality. Tradition. Excellence.</title>
+</head>
+
+<body>
+
+  <?php
+  include 'navbar.php';
+  ?>
+
+  <div class="product-header">
+    <div class="product-caption container">
+      <h1>Our Product</h1>
+    </div>
+  </div>
+  
+  <section class="products">
+    <div class="product-nav">
+      <div class="product-nav-items container-lg">
+        <div class="product-nav-links">All</div>
+        <div class="product-nav-links">Coffee</div>
+        <div class="product-nav-links">Pappadam</div>
+        <div class="product-nav-links">Vadam</div>
+        <div class="product-nav-links">Karuvada</div>
+        <div class="product-nav-links">Kondattam</div>
+      </div>
+    </div>
+    <div class="products-container container-lg">
+      <div class="products-flex-box container-lg row">
+
+        <div class="card col-md-4" style="width: 15.9rem;">
+          <img src="assets\product1.jpg" class="card-img-top" alt="product1">
+          <div class="card-body">
+            <p class="card-catagory">Cat-name</p>
+            <h5 class="card-title">Narasu's Dhall Rice Powder</h5>
+            <span class="card-price">&#8377; 20.00</span>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn card-btn">Add to Cart</a>
+          </div>
+        </div>
+
+        <div class="card col-md-4" style="width: 15.9rem;">
+          <img src="assets\product1.jpg" class="card-img-top" alt="product1">
+          <div class="card-body">
+          <p class="card-catagory">Cat-name</p>
+            <h5 class="card-title">Narasu's Dhall Rice Powder</h5>
+            <span class="card-price">&#8377; 20.00</span>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn card-btn">Add to Cart</a>
+          </div>
+        </div>
+
+        <div class="card col-md-4" style="width: 15.9rem;">
+          <img src="assets\product1.jpg" class="card-img-top" alt="product1">
+          <div class="card-body">
+          <p class="card-catagory">Cat-name</p>
+            <h5 class="card-title">Narasu's Dhall Rice Powder</h5>
+            <span class="card-price">&#8377; 20.00</span>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn card-btn">Add to Cart</a>
+          </div>
+        </div>
+
+        <div class="card col-md-4" style="width: 15.9rem;">
+          <img src="assets\product1.jpg" class="card-img-top" alt="product1">
+          <div class="card-body">
+          <p class="card-catagory">Cat-name</p>
+            <h5 class="card-title">Narasu's Dhall Rice Powder</h5>
+            <span class="card-price">&#8377; 20.00</span>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn card-btn">Add to Cart</a>
+          </div>
+        </div>
+        
+        <div class="card col-md-4" style="width: 15.9rem;">
+          <img src="assets\product1.jpg" class="card-img-top" alt="product1">
+          <div class="card-body">
+          <p class="card-catagory">Cat-name</p>
+            <h5 class="card-title">Narasu's Dhall Rice Powder</h5>
+            <span class="card-price">&#8377; 20.00</span>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn card-btn">Add to Cart</a>
+          </div>
+        </div>
+
+        <div class="card col-md-4" style="width: 15.9rem;">
+          <img src="assets\product1.jpg" class="card-img-top" alt="product1">
+          <div class="card-body">
+          <p class="card-catagory">Cat-name</p>
+            <h5 class="card-title">Narasu's Dhall Rice Powder</h5>
+            <span class="card-price">&#8377; 20.00</span>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn card-btn">Add to Cart</a>
+          </div>
+        </div>
+
+        <div class="card col-md-4" style="width: 15.9rem;">
+          <img src="assets\product1.jpg" class="card-img-top" alt="product1">
+          <div class="card-body">
+          <p class="card-catagory">Cat-name</p>
+            <h5 class="card-title">Narasu's Dhall Rice Powder</h5>
+            <span class="card-price">&#8377; 20.00</span>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn card-btn">Add to Cart</a>
+          </div>
+        </div>
+
+        <div class="card col-md-4" style="width: 15.9rem;">
+          <img src="assets\product1.jpg" class="card-img-top" alt="product1">
+          <div class="card-body">
+          <p class="card-catagory">Cat-name</p>
+            <h5 class="card-title">Narasu's Dhall Rice Powder</h5>
+            <span class="card-price">&#8377; 20.00</span>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+              content.</p>
+            <a href="#" class="btn card-btn">Add to Cart</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <?php
+    include 'footer.php'
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
